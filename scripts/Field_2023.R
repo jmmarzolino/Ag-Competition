ec2392e4421ff768014366be4867281fb7bec003
jmmarzolino <jmmarzolino@ucdavis.edu>
2023-07-06 12:52:04 -0700

    set working directory and write out file

--- scripts/Field_2023.R
+++ scripts/Field_2023.R
@@ -4,12 +4,16 @@ library(dplyr)
 library(ggpubr)
 library(ggplot2)
 
-df <- read_delim("~/Desktop/FT_DAYS_2022-2023.xlsx - FIELD_LAYOUT.tsv",
+setwd("~/Desktop/")
+
+df <- read_delim("FT_DAYS_2022-2023.xlsx - FIELD_LAYOUT.tsv",
                  delim = "\t", escape_double = F)
-df1 <- read_delim("~/Desktop/Seed Weights - Field 2023.tsv",
+df1 <- read_delim("Seed Weights - Field 2023.tsv",
                   delim = "\t", escape_double = F)
 
 dfs <- full_join(df, df1, by = ("PLOT_ID"))
 
 
+
+write_delim(dfs, "Seed_Weights_to_Positions.tsv")
 
