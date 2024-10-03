54ee136628d444da6cdc38e9bf12d234a177e1cd
Jill Marzolino <jmarz001@ucr.edu>
2024-10-03 11:31:46 -0700

    generalizing functions

--- scripts/CUSTOM_FNS.R
+++ scripts/CUSTOM_FNS.R
@@ -1,7 +1,7 @@
 # add a generation column based on genotype UCRKL numbers
 
 add_generation <- function(x){
-  x$Generation <- gsub("^1_.*", 18, x$Genotypes)
+  x$Generation <- gsub("^1_.*", 18, x$Genotype)
   x$Generation <- gsub("^2_.*", 28, x$Generation)
   x$Generation <- gsub("^3_.*", 50, x$Generation)
   x$Generation <- gsub("^7_.*", 58, x$Generation)
@@ -14,20 +14,19 @@ Exp_Single <- function(x){
   return(result_single)
 }
 
-Exp_Fec_Mixed <- function(x){
-  TW_mix <- (x/2) + (Averaged_Full_2021_2022$Atlas_Avg_Fec/2)
-  Exp_Fec_mix <- TW_mix/10
-  return(Exp_Fec_mix)
+Exp_Mixed <- function(x, y){
+  # note - x and y must be table columns or vectors
+  Exp_mix <- ((x/2) + (y/2))/10
+  return(Exp_mix)
 }
 
-Exp_Fit_Mixed <- function(x){
-  fit_mix <- (x/2) + (Averaged_Full_2021_2022$Atlas_Avg_Fit/2)
-  Exp_Fit_mix <- fit_mix/10
-  return(Exp_Fit_mix)
-}
 
-Exp_TW_mix <- function(x){
-  TW_mix <- (x/2) + (Average_Haplo_rep$Atlas_Avg_Total_Weight/2)
-  Exp_TW <- TW_mix/10
-  return(Exp_TW)
-}
+
+#Expected_seed <- function(x, y){
+#  separate the rows
+#  if(grep("single")){
+#    aapoigapoiefd
+#  }
+#  if(grep("mixed")){}
+#    else("print error")
+#}
