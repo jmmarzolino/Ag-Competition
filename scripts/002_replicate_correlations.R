5782f83c4b9ac54940d58c1226b79dfaf4a5c46a
Jill Marzolino <jmarz001@ucr.edu>
2024-10-08 19:11:12 -0700

    plot residuals

--- scripts/002_replicate_correlations.R
+++ scripts/002_replicate_correlations.R
@@ -9,15 +9,20 @@
 library(tidyverse)
 library(ggpubr)
 library(data.table)
+library(ggrepel)
 
 library(gridExtra)
 library(car)
-library(ggrepel)
+
 
 
 setwd("/rhome/jmarz001/bigdata/Ag-Competition/data")
 # load data
 df <- fread("JOINED_PHENOTYPES.tsv")
+## temporary code to identify extreme seed weight values
+df <- df %>% filter(SEED_WEIGHT_100 < 30)
+
+
 df1 <- df %>% filter(Exp_year==2022)
 df2 <- df %>% filter(Exp_year==2023)
 
@@ -67,22 +72,19 @@ replicate_df <- df %>%
   pivot_longer(cols=c(FT, TOTAL_MASS, SEED_WEIGHT_100), names_to='PHENOTYPE', values_to="VALUE") %>%
   pivot_wider(names_from=Replicate, names_prefix="REP_", values_from='VALUE')
 
+# another way of summarizing trait replicate correlations by year, condition, and phenotype
+replicate_df %>% group_by(Exp_year, Condition, PHENOTYPE) %>% summarise('correlation'=cor(REP_1, REP_2, use="pairwise.complete.obs"))
 
 
 
-tm <- df %>%
-  filter(Plants>0 & !is.na(TOTAL_MASS) & !is.na(SEED_WEIGHT_100)) %>% 
-  select(-c(Plants, BED, ROW)) %>%
+
+#tm <- df %>%
+ # filter(Plants>0 & !is.na(TOTAL_MASS) & !is.na(SEED_WEIGHT_100)) %>% 
+ # select(-c(Plants, BED, ROW)) %>%
   #filter(Condition=="single") %>%
   #pivot_longer(cols=c(FT, TOTAL_MASS, SEED_WEIGHT_100), names_to='PHENOTYPE', values_to="VALUE") %>%
-  select(c(Genotype, Condition, Replicate, Exp_year, TOTAL_MASS)) %>% 
-  pivot_wider(names_from=Replicate, names_prefix="REP_", values_from='TOTAL_MASS')
-
-
-
-
-
-
+ # select(c(Genotype, Condition, Replicate, Exp_year, TOTAL_MASS)) %>% 
+ # pivot_wider(names_from=Replicate, names_prefix="REP_", values_from='TOTAL_MASS')
 
 
 # Creating a function to graph correlations between replicates
@@ -112,9 +114,9 @@ c1 <- graph_correlation(df)
 c2 <- graph_correlation(df, y='SEED_WEIGHT_100') 
 c3 <- graph_correlation(df, y='FT') 
 
-ggsave(c1, "../results/replicate_correlations_total_mass.png", width = 12, height = 12, dpi=300)
-ggsave(c2, "../results/replicate_correlations_seed_weight_100.png", width = 12, height = 12, dpi=300)
-ggsave(c3, "../results/replicate_correlations_FT.png", width = 12, height = 12, dpi=300)
+ggsave("../results/replicate_correlations_total_mass.png", c1, width = 12, height = 12, dpi=300)
+ggsave("../results/replicate_correlations_seed_weight_100.png", c2, width = 12, height = 12, dpi=300)
+ggsave("../results/replicate_correlations_FT.png", c3, width = 12, height = 12, dpi=300)
 
 png("../results/replicate_correlations_all.png")
 ggarrange(c1, c2, c3)
@@ -123,68 +125,78 @@ dev.off()
 
 
 
-## temporary code to identify extreme seed weight values
-df %>% filter(SEED_WEIGHT_100 >30)
 
 
 
 
 
-
-
-
-#p1 <- graph_correlation(cmp, "Total Weight (g)", "2021-2022") +
-#  stat_cor(label.y = 170)
-#p2 <- graph_correlation(cmp, "Relative Fecundity", "2021-2022") +
-#  stat_cor(label.y = 2.5, label.x = 6.5) +
-#  geom_text_repel(label = ifelse(cmp$REP_1 > 3 | cmp$REP_2 > 3,
- #                                cmp$Genotype,
-  #                               ""), size = 3, hjust =1, max.overlaps = 30)
-
-
-
-upper <- median(mix1$TOTAL_MASS, na.rm = T) + (2 * IQR(mix1$TOTAL_MASS, na.rm = T))
-lower <- median(mix1$TOTAL_MASS, na.rm = T) - (2 * IQR(mix1$TOTAL_MASS, na.rm = T))
-outlier_data <- mix1 %>% filter(TOTAL_MASS > upper | TOTAL_MASS < lower)
 
 
 
 
 
 # Residual Plotting
-cmp <- PHENO_MIXED_2022 %>% group_by(Genotype, Generation, Replicate) %>% summarise(TOTAL_MASS) %>% spread(key = Replicate, value = TOTAL_MASS) %>% ungroup()
+
+## scale data
+scaled <- df %>% select(-c(Plants, BED, ROW)) 
+scaled$FT <- scale(scaled$FT)[,1]
+scaled$TOTAL_MASS <- scale(scaled$TOTAL_MASS)[,1]
+scaled$SEED_WEIGHT_100 <- scale(scaled$SEED_WEIGHT_100)[,1]
 
 
-plot_residuals <- function(cmp, lab_x="", lab_title="Residual Plot") {
-    cmp <- na.omit(cmp)
-    residual_df <- cmp %>% reframe(Residuals = resid(lm(REP_2 ~ REP_1)))
-    cmp$Residuals <- residual_df$Residuals
-    Standard_dev <- sd(cmp$Residuals)
-    cmp <- cmp %>% mutate(Residuals = Residuals/Standard_dev)
-    tmp_outlier <- cmp %>% filter(Residuals > 2 | Residuals < -2)
+plot_residuals <- function(scaled, y='TOTAL_MASS', condition="single", year=2022) {
+    scaled <- na.omit(scaled)
+    sc2 <- scaled %>% 
+        filter(Condition == condition) %>%
+        select(c(Genotype, Replicate, Exp_year, y)) %>%
+        pivot_wider(names_from=Replicate, names_prefix="REP_", values_from=y)
 
-    g <- ggplot(cmp, aes(x = REP_1, y = Residuals)) +
+    sc_22 <- sc2 %>% filter(Exp_year == year) %>% select(-Exp_year) %>% na.omit()
+
+    d <- sc_22 %>% reframe("RES" = resid(lm(REP_2 ~ REP_1))) 
+    sc_22$RES <- d$RES / sd(d$RES)
+    print(sc_22 %>% filter(RES > 2 | RES < -2))
+
+
+    g1 <- ggplot(sc_22, aes(x = REP_1, y = RES)) +
       geom_jitter() +
-      geom_hline(yintercept = 0, color = "red", line_type="dashed") +
+      geom_hline(yintercept = 0, color = "red") +
       geom_hline(yintercept = 2, color = "blue") +
       geom_hline(yintercept = -2, color = "blue") +
       scale_y_continuous(breaks = seq(-4, 4, 1)) +
-      geom_text_repel(label = ifelse(cmp$Residuals > 2 | cmp$Residuals < -2,
-                                    cmp$Genotype,
-                                    ""), size = 3, hjust = 1, max.overlaps = 30) +
-      labs(x = lab_x,
-          title = lab_title)
+      ylim(c(-4,4)) +
+      geom_text_repel(label = ifelse(sc_22$RES > 2 | sc_22$RES < -2,
+                                scaled$Genotype, ""), 
+                                size = 3, hjust = 1, max.overlaps = 30) +
+      labs(x = "Replicate 1", y = "Residuals", title = paste0("Residual Plot - ", year, ", ", condition), subtitle = y)
 
-    return(g)
+    # g2 is just a qq plot
+    #g2 <- ggplot(sc_22, aes(x = REP_2, y = RES)) +
+     # geom_jitter() +
+     # geom_hline(yintercept = 0, color = "red") +
+     # geom_hline(yintercept = 2, color = "blue") +
+     # geom_hline(yintercept = -2, color = "blue") +
+     # scale_y_continuous(breaks = seq(-4, 4, 1)) +
+     # ylim(c(-4,4)) +
+     # geom_text_repel(label = ifelse(sc_22$RES > 2 | sc_22$RES < -2,
+     #                           scaled$Genotype, ""), 
+     #                           size = 3, hjust = 1, max.overlaps = 30) +
+     # labs(x = "Replicate 2", y = "Residuals", title = paste0("Residual Plot - ", year, ", ", condition), subtitle = y)
+
+    #g <- ggarrange(g1, g2)
+    return(g1)
 }
 
-a <- plot_residuals(cmp, lab_x="Centered Fecundity", lab_title="Residual Plot - Mixed, Centered Fecundity, 2022")
-a <- plot_residuals(cmp, lab_x="Total Weight (grams)", lab_title="Residual Plot - Mixed, Total Weight, 2022")
 
+y=c('TOTAL_MASS', 'FT', 'SEED_WEIGHT_100')
 
+for(i in 1:3){
+    p1 <- plot_residuals(scaled, y=y[i], condition="single", year=2022)
+    p2 <- plot_residuals(scaled, y=y[i], condition="mixed", year=2022)
 
+    p3 <- plot_residuals(scaled, y=y[i], condition="single", year=2023)
+    p4 <- plot_residuals(scaled, y=y[i], condition="mixed", year=2023)
 
-
-
-y <- arrangeGrob(a,b,c,d,e,f,g,h,i,j, nrow = 2, ncol = 5)
-ggsave("../results/residuals.png", y, width = 26, height =14)
+    ppp <- ggarrange(p1, p2, p3, p4, ncol=2, nrow=2)
+    ggsave(paste0("../results/residuals_", y[i], ".png"), ppp, width=20, height=20, dpi=300)
+}
