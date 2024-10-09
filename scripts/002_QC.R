6a15ca34bcb1fb4fd1da52023b3f3db03e1daab2
Jill Marzolino <jmarz001@ucr.edu>
2024-10-08 19:11:39 -0700

    notes

--- scripts/002_QC.R
+++ scripts/002_QC.R
@@ -40,6 +40,26 @@ ggsave("results/plot_survival.png")#, width = 12, height = 10)
 
 
 
+#p1 <- graph_correlation(cmp, "Total Weight (g)", "2021-2022") +
+#  stat_cor(label.y = 170)
+#p2 <- graph_correlation(cmp, "Relative Fecundity", "2021-2022") +
+#  stat_cor(label.y = 2.5, label.x = 6.5) +
+#  geom_text_repel(label = ifelse(cmp$REP_1 > 3 | cmp$REP_2 > 3,
+ #                                cmp$Genotype,
+  #                               ""), size = 3, hjust =1, max.overlaps = 30)
+
+
+
+upper <- median(mix1$TOTAL_MASS, na.rm = T) + (2 * IQR(mix1$TOTAL_MASS, na.rm = T))
+lower <- median(mix1$TOTAL_MASS, na.rm = T) - (2 * IQR(mix1$TOTAL_MASS, na.rm = T))
+outlier_data <- mix1 %>% filter(TOTAL_MASS > upper | TOTAL_MASS < lower)
+
+
+
+
+
+
+
 
 ### Extrememly high 100 seed weights
 
