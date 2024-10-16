9b0fa86e9391d76dbb91fbadd9fe17e5a3355657
Jill Marzolino <jmarz001@ucr.edu>
2024-10-15 18:19:00 -0700

    call custom functions

--- scripts/002_derived_traits_and_QC.R
+++ scripts/002_derived_traits_and_QC.R
@@ -12,6 +12,8 @@ library(data.table)
 #library(dunn.test)
 
 setwd("/bigdata/koeniglab/jmarz001/Ag-Competition")
+source("../scripts/CUSTOM_FNS.R")
+
 
 df <- read_delim("data/JOINED_PHENOTYPES.tsv")
 # remove any rows without genotype
