d268f19fecb0a4734a4fb2d899cd037953de4c99
Jill Marzolino <jmarz001@ucr.edu>
2024-10-17 13:41:49 -0700

    add plots to list & save out as one combined set of plots

--- scripts/plotting/indv_trait_barplots.R
+++ scripts/plotting/indv_trait_barplots.R
@@ -6,11 +6,11 @@
 
 library(tidyverse)
 library(ggpubr)
+library(data.table)
 
 setwd("/bigdata/koeniglab/jmarz001/Ag-Competition")
 df <- fread("data/FITNESS.tsv")
 
-pdf("results/barplot_trait_vals_per_genotype.pdf")
 
 
 collist <- c('FT', 'TOTAL_MASS', 'SEED_WEIGHT_100', 'SURVIVAL', 'SEED_COUNT', 'FECUNDITY',  'FITNESS', 'RELATIVE_FITNESS', 'AT_REL_FITNESS')
@@ -20,11 +20,13 @@ for(trait in collist) {
     g <- ggplot(df, aes(x=reorder(Genotype, get(trait)), y=get(trait), fill=as.factor(Generation))) +
             geom_col() +
             facet_wrap(~Condition) +
-            theme_bw() +
+            theme_bw(base_size = 12) +
+            theme(axis.text.x = element_text(angle = 90)) +
             labs(x="Genotype", y=trait, fill = "Generation")
-    print(g)
+    assign(paste0("g_", trait), g)
 
 }
 
+g_all <- ggarrange(g_FT, g_TOTAL_MASS, g_SEED_WEIGHT_100, g_SURVIVAL, g_SEED_COUNT, g_FECUNDITY, g_FITNESS, g_RELATIVE_FITNESS, g_AT_REL_FITNESS, ncol=3, nrow=3)
 
-dev.off()
+ggsave("results/barplot_trait_vals_per_genotype.png", g_all, height=20, width=20)
