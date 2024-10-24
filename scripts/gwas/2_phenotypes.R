4e730dda18a88ec8c6e287a3b3db571034bda1ac
Jill Marzolino <jmarz001@ucr.edu>
2024-10-24 12:55:04 -0700

    scale data and remove redundant trait cols

--- scripts/gwas/2_phenotypes.R
+++ scripts/gwas/2_phenotypes.R
@@ -7,6 +7,7 @@
 
 library(tidyverse)
 library(data.table)
+library(corrplot)
 
 setwd("/rhome/jmarz001/bigdata/Ag-Competition/results/gwas")
 # read in phenotypes file
@@ -22,6 +23,22 @@ trait_names_list <- colnames(pheno)[3:ncol(pheno)]
 trait_num_list <- 6:(length(trait_names_list)+5)
 trait_n_list <- tibble(trait_names_list, trait_num_list)
 write_delim(trait_n_list, "trait_name_to_col_numbers.tsv")
+
+# scale the phenotypes
+pheno <- pheno %>% mutate(across(-c(Genotype, Generation), ~(scale(.) %>% as.vector), .names="{.col}_scaled")) %>% select(c(Genotype, Generation, ends_with("_scaled")))
+
+
+
+# remove highly correlated phenotypes from gwas
+# fitness / atlas-fitness / 
+traits_df <- pheno %>% select(c(ends_with("_scaled"))) 
+#traits_df <- pheno %>% select(c(ends_with("_scaled"))) %>% select(-c(SEED_COUNT_scaled, RELATIVE_FITNESS_scaled, AT_REL_FITNESS_scaled)) 
+
+x <- cor(traits_df, use="na.or.complete", method="spearman")
+corrplot(x, method="color", type="upper", order="original", title="", mar=c(0,0,4,0), addCoef.col = "black")
+
+# remove highly correlated phenotypes 
+pheno <- pheno %>% select(-c(SEED_COUNT_scaled, RELATIVE_FITNESS_scaled, AT_REL_FITNESS_scaled)) 
 
 
 ## make genotypes match between genotype and phenotype files
