05e46a5ac6a2301caebd8d754f2d897e9b7c3772
Jill Marzolino <jmarz001@ucr.edu>
2024-10-23 17:04:57 -0700

    differentiate univariate and multivariate gwas scripts

---
+++ scripts/gwas/3_multivariate_association.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=gwas
+#SBATCH --output=/rhome/jmarz001/bigdata/Ag-Competition/scripts/gwas/3_multivariate_association.stdout
+#SBATCH --ntasks=4
+#SBATCH --mem=100G
+#SBATCH -p koeniglab
+
+# GEMMA 0.98
+cd /rhome/jmarz001/bigdata/Ag-Competition/results
+
+# define variables
+GENO=all_traits
+KINSHIP=output/related_matrix.cXX.txt
+PCS=pca.txt
+OUT=gwas
+PHENO_NAME="MULI_PHENO"
+
+### Univariate Linear Mixed Model
+# -km 1: PLINK binary ped as relatedness matrix; 1 is default
+# -k [file] specifies the relatedness matrix file name
+# if no covar use lm instead of lmm
+# -c [file] specify input covariates file name (optional); an intercept term is needed in the covariates file
+
+#/rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -k "$KINSHIP" -c "$PCS" -lmm 4 -miss 1 -notsnp -outdir $OUT -n $SLURM_ARRAY_TASK_ID -o $PHENO_NAME -debug
+
+
+### Multivariate Linear Mixed model
+#-n [num1] [num2] [num3]
+#-n option is employed to specify which phenotypes in the phenotype file are used for association tests
+
+# restrict the number of phenotypes to be < ten
+#when a small proportion of phenotypes are partially missing, one can impute these missing values before association tests w/ -predict
+
+
+/rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -k "$KINSHIP" -c "$PCS" -lmm 4 -outdir $OUT -n 1 2 3 4 5 6 7 8 9 -o $PHENO_NAME -debug
---
+++ scripts/gwas/3_univariate_association_array.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=gwas
+#SBATCH --output=/rhome/jmarz001/bigdata/Ag-Competition/scripts/gwas/3_univariate_association_array.stdout
+#SBATCH --ntasks=2
+#SBATCH --mem=80gb
+#SBATCH -p koeniglab
+
+# GEMMA 0.98
+cd /rhome/jmarz001/bigdata/Ag-Competition/results
+
+# define variables
+GENO=all_traits
+KINSHIP=output/related_matrix.cXX.txt
+PCS=pca.txt
+OUT=gwas
+COL=$(expr $SLURM_ARRAY_TASK_ID + 5)
+
+PHENO_NAME="ASSOC_${COL}"
+
+### Univariate Linear Mixed Model
+# -km 1: PLINK binary ped as relatedness matrix; 1 is default
+# -k [filename] specifies the relatedness matrix file name
+#/rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -k "$KINSHIP" -c "$PCS" -lmm 4 -miss 1 -notsnp -outdir $OUT -n $SLURM_ARRAY_TASK_ID -o $PHENO_NAME -debug
+
+## if no covar use lm instead of lmm
+#One can specify a different column as the phenotype column by using “-n [num]”, where ”-n 1” uses the original sixth column as phenotypes, and “-n 2” uses the seventh column, and so on and so forth.
+
+/rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -k "$KINSHIP" -c "$PCS" -lmm 4 -outdir $OUT -n $SLURM_ARRAY_TASK_ID -o $PHENO_NAME -debug
