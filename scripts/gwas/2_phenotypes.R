b1899b2d764b580b2f3c965e3d6915473e679028
Jill Marzolino <jmarz001@ucr.edu>
2024-10-24 13:03:56 -0700

    reorder code chunks

--- scripts/gwas/2_phenotypes.R
+++ scripts/gwas/2_phenotypes.R
@@ -18,27 +18,18 @@ pheno <- pheno %>% filter(Generation != 0)
 # filter out 'mixed' condition plots
 pheno <- pheno %>% filter(Condition != "mixed") %>% select(-Condition)
 
+# scale the phenotypes
+pheno <- pheno %>% mutate(across(-c(Genotype, Generation), ~(scale(.) %>% as.vector), .names="{.col}_scaled")) %>% select(c(Genotype, Generation, ends_with("_scaled")))
+
+# remove highly correlated phenotypes 
+pheno <- pheno %>% select(-c(SEED_COUNT_scaled, RELATIVE_FITNESS_scaled, AT_REL_FITNESS_scaled)) 
+
+
 # record traits and corresponding col number
 trait_names_list <- colnames(pheno)[3:ncol(pheno)]
 trait_num_list <- 6:(length(trait_names_list)+5)
 trait_n_list <- tibble(trait_names_list, trait_num_list)
 write_delim(trait_n_list, "trait_name_to_col_numbers.tsv")
-
-# scale the phenotypes
-pheno <- pheno %>% mutate(across(-c(Genotype, Generation), ~(scale(.) %>% as.vector), .names="{.col}_scaled")) %>% select(c(Genotype, Generation, ends_with("_scaled")))
-
-
-
-# remove highly correlated phenotypes from gwas
-# fitness / atlas-fitness / 
-traits_df <- pheno %>% select(c(ends_with("_scaled"))) 
-#traits_df <- pheno %>% select(c(ends_with("_scaled"))) %>% select(-c(SEED_COUNT_scaled, RELATIVE_FITNESS_scaled, AT_REL_FITNESS_scaled)) 
-
-x <- cor(traits_df, use="na.or.complete", method="spearman")
-corrplot(x, method="color", type="upper", order="original", title="", mar=c(0,0,4,0), addCoef.col = "black")
-
-# remove highly correlated phenotypes 
-pheno <- pheno %>% select(-c(SEED_COUNT_scaled, RELATIVE_FITNESS_scaled, AT_REL_FITNESS_scaled)) 
 
 
 ## make genotypes match between genotype and phenotype files
@@ -50,7 +41,6 @@ plink_file <- plink_file[,-6]
 # & keep only overlapping genotypes
 joined <- inner_join(plink_file, pheno, by=c('V2'='Genotype'))
 #left_join
-
 
 #### over-write formatted genotype and phenotype .fam file
 ## format = famID / indvID / <phenotype / cols /...>
