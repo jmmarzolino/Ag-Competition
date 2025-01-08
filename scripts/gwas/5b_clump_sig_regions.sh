a3ec3977ac137b79d018c1bafb454a6ffb0b1d74
Jill Marzolino <jmarz001@ucr.edu>
2025-01-08 15:15:28 -0800

    fix file location

--- scripts/gwas/5b_clump_sig_regions.sh
+++ scripts/gwas/5b_clump_sig_regions.sh
@@ -11,4 +11,4 @@ module load plink/1.90b6.25
 ls ASSOC_*assoc.tmp > tmp.txt #backup record of file order fed into plink
 FILE_LIST=$(ls ASSOC_*assoc.tmp | grep -v / | xargs echo | sed 's/ /,/g')
 
-plink --bfile ../all_traits --double-id --allow-no-sex --allow-extra-chr --clump $FILE_LIST
+plink --bfile all_traits --double-id --allow-no-sex --allow-extra-chr --clump $FILE_LIST
