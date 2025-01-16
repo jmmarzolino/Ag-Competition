9b45cf3e51e3e5c64df5becb40a0f09f200625cd
Jill Marzolino <jmarz001@ucr.edu>
2025-01-16 12:01:26 -0800

    update obj name

--- scripts/003a_H2_BLUPs.R
+++ scripts/003a_H2_BLUPs.R
@@ -14,21 +14,20 @@ source("../scripts/CUSTOM_FNS.R")
 # Loading Data
 pheno <- read_delim("DERIVED_PHENOTYPES.tsv")
 
-pheno_scale <- pheno %>% 
+pheno <- pheno %>% 
     filter(Condition == "single") %>% 
-    select(c(Genotype, Generation, Replicate, Exp_year, FT, TOTAL_MASS, GERMINATION, SEED_WEIGHT_100, FECUNDITY, FITNESS)) #%>% 
-    #mutate(across(-c(Genotype, Generation, Replicate, GERMINATION), ~(scale(.) %>% as.vector))) 
+    select(c(Genotype, Generation, Replicate, Exp_year, FT, TOTAL_MASS, GERMINATION, SEED_WEIGHT_100, FECUNDITY, FITNESS)) %>% 
+    mutate(across(-c(Genotype, Generation, Replicate, GERMINATION), ~(scale(.) %>% as.vector)))
 
 
 ## extract blups & H2 for each trait & model
-
 
 # init pdf to store plot outputs
 pdf("heritability_model_fits.pdf", width=20, height=10)
 
 ################### FLOWERING TIME
 h2_ft <- 
-H2cal(data = pheno_scale
+H2cal(data = pheno
           , trait = "FT"
           , gen.name = "Genotype"
           , rep.n = 2
@@ -42,12 +41,12 @@ H2cal(data = pheno_scale
           )
 
 
-#lmer(FT ~ 1 + (1|Genotype) + (1|Genotype:Exp_year), pheno_scale) %>% plot
+#lmer(FT ~ 1 + (1|Genotype) + (1|Genotype:Exp_year), pheno) %>% plot
 
-#mod <-  lmer(FT ~ 1 + Exp_year + (1|Genotype), pheno_scale)
-#pheno_scale$resids <- resid(mod)
-#pheno_scale$fitted <- fitted(mod)
-#ggplot(pheno_scale, aes(fitted, resids, color=GERMINATION)) + geom_point() + geom_smooth() + geom_hline(yintercept=0)
+#mod <-  lmer(FT ~ 1 + Exp_year + (1|Genotype), pheno)
+#pheno$resids <- resid(mod)
+#pheno$fitted <- fitted(mod)
+#ggplot(pheno, aes(fitted, resids, color=GERMINATION)) + geom_point() + geom_smooth() + geom_hline(yintercept=0)
 
 
 ## Extract intercept + genotype data to re-scale data to original units
@@ -59,7 +58,7 @@ h2_ft$blups$FT <- h2_ft$blups$FT + inter
 
 ################### TOTAL MASS
 h2_totmass <- 
-H2cal(data = pheno_scale
+H2cal(data = pheno
           , trait = "TOTAL_MASS"
           , gen.name = "Genotype"
           , rep.n = 2
@@ -73,9 +72,9 @@ H2cal(data = pheno_scale
           )
 
 
-#lmer(TOTAL_MASS ~ 1 + Exp_year + GERMINATION + Replicate + (1|Genotype) + (1|Genotype:Exp_year), pheno_scale) 
+#lmer(TOTAL_MASS ~ 1 + Exp_year + GERMINATION + Replicate + (1|Genotype) + (1|Genotype:Exp_year), pheno) 
 
-#lmer(TOTAL_MASS ~ 1 + Exp_year + FITNESS + Generation + Replicate + (1|Genotype) + (1|Genotype:Exp_year), pheno_scale)
+#lmer(TOTAL_MASS ~ 1 + Exp_year + FITNESS + Generation + Replicate + (1|Genotype) + (1|Genotype:Exp_year), pheno)
 
 inter <- summary(h2_totmass$model)$coefficients[1, 1]
 h2_totmass$blups$TOTAL_MASS <- h2_totmass$blups$TOTAL_MASS + inter
@@ -83,7 +82,7 @@ h2_totmass$blups$TOTAL_MASS <- h2_totmass$blups$TOTAL_MASS + inter
 
 
 h2_sw100 <- 
-H2cal(data = pheno_scale
+H2cal(data = pheno
           , trait = "SEED_WEIGHT_100"
           , gen.name = "Genotype"
           , rep.n = 2
@@ -104,7 +103,7 @@ h2_sw100$blups$SEED_WEIGHT_100 <- h2_sw100$blups$SEED_WEIGHT_100 + inter
 
 ## not fitable
 h2_germ <- 
-H2cal(data = pheno_scale
+H2cal(data = pheno
         , trait = "GERMINATION"
         , gen.name = "Genotype"
         , rep.n = 2
@@ -122,7 +121,7 @@ h2_germ$blups$GERMINATION <- h2_germ$blups$GERMINATION + inter
 
 
 h2_fec <- 
-H2cal(data = pheno_scale
+H2cal(data = pheno
           , trait = "FECUNDITY"
           , gen.name = "Genotype"
           , rep.n = 2
@@ -134,11 +133,11 @@ H2cal(data = pheno_scale
           , outliers.rm = TRUE
           )
 
-#mod <- lmer(FECUNDITY ~ 1 + GERMINATION + FT + (1|Genotype) + (1|Genotype:Exp_year), pheno_scale)
-#pheno_scale$resids <- resid(mod)
-#pheno_scale$fits <- fitted(mod)
+#mod <- lmer(FECUNDITY ~ 1 + GERMINATION + FT + (1|Genotype) + (1|Genotype:Exp_year), pheno)
+#pheno$resids <- resid(mod)
+#pheno$fits <- fitted(mod)
 
-#ggplot(pheno_scale, aes(fitted, resids)) + geom_point() + geom_smooth() + geom_hline(yintercept=0)
+#ggplot(pheno, aes(fitted, resids)) + geom_point() + geom_smooth() + geom_hline(yintercept=0)
 
 inter <- summary(h2_fec$model)$coefficients[1, 1]
 h2_fec$blups$FECUNDITY <- h2_fec$blups$FECUNDITY + inter
@@ -147,7 +146,7 @@ h2_fec$blups$FECUNDITY <- h2_fec$blups$FECUNDITY + inter
 
 
 h2_fit <- 
-H2cal(data = pheno_scale
+H2cal(data = pheno
         , trait = "FITNESS"
         , gen.name = "Genotype"
         , rep.n = 2
@@ -166,7 +165,7 @@ h2_fit$blups$FITNESS <- h2_fit$blups$FITNESS + inter
 dev.off()
 
 
-H2_table <- tibble("trait" = colnames(pheno_scale)[5:ncol(pheno_scale)], 
+H2_table <- tibble("trait" = colnames(pheno)[5:ncol(pheno)], 
                     "H2_s" = c(h2_ft$tabsmr$H2.s, h2_totmass$tabsmr$H2.s, h2_sw100$tabsmr$H2.s, h2_germ$tabsmr$H2.s, h2_fec$tabsmr$H2.s, h2_fit$tabsmr$H2.s) )
 write_delim(H2_table, "trait_heritability.tsv", "\t")
 
