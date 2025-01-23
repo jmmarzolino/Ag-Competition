e07e4ed9ade9048be2a4d50dfe77f00573b5735d
Jill Marzolino <jmarz001@ucr.edu>
2025-01-23 10:29:42 -0800

    simplify plotting code

--- scripts/plotting/4_traits_between_haplotypes.R
+++ scripts/plotting/4_traits_between_haplotypes.R
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=30G
 #SBATCH --time=02:00:00
-#SBATCH --output=/rhome/jmarz001/bigdata/Ag-Competition/scripts/4_traits_between_haplotypes.stdout
+#SBATCH --output=/rhome/jmarz001/bigdata/Ag-Competition/scripts/plotting/4_traits_between_haplotypes.stdout
 #SBATCH -p koeniglab
 
 
@@ -12,89 +12,77 @@ library(ggpubr)
 library(ggplot2)
 
 setwd("/bigdata/koeniglab/jmarz001/Ag-Competition/data")
-fitness_df <- read_delim("FITNESS.tsv")
-
-### 5a_Comparing_Yield_Between_Haplotypes.R
-
-Rep_Single <- Rep_Single %>% filter(Haplotype != "NA")
-Rep_Single$Haplotype <- as.factor(Rep_Single$Haplotype)
-Haplo_graph <- Rep_Single %>% group_by(Generation, Haplotype) %>% summarise(Avg_TW = mean(TOTAL_WEIGHT)) %>% ungroup()
-Haplo_graph <- Haplo_graph %>% group_by(Generation) %>% mutate(GenAvg = mean(Avg_TW)) %>% ungroup()
-Haplo_graph$`Avg Yield > Pop & Gen Avg Yield` <- ifelse(Haplo_graph$Avg_TW > Haplo_graph$GenAvg & Haplo_graph$Avg_TW > mean(Haplo_graph$Avg_TW), TRUE, FALSE)
-f_AY <- Haplo_graph %>% filter(`Avg Yield > Pop & Gen Avg Yield` == TRUE) %>% filter(!duplicated(Haplotype)) %>% select(Haplotype)
-
-ggplot(Haplo_graph,aes(x= Haplotype, y = Avg_TW, fill = `Avg Yield > Pop & Gen Avg Yield`)) +
-  geom_bar(stat = "identity") +
-  geom_hline(yintercept = mean(Haplo_graph$Avg_TW), color = "green") +
-  geom_hline(aes(yintercept = GenAvg), color = "blue", linetype = 2) +
-  labs(y = "Average Total Seed Weight (g)",
-       title = "Average Total Seed Weight between Haplotypes") +
-  scale_y_continuous(breaks = seq(0, 200, 25)) +
-  facet_wrap(~Generation, scales = "free_x") + 
-  theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(values = c("black", "red")) 
-ggsave("scripts/plotting/05a_Average_TW_Haplotypes_2022_2023.png", width = 16, height = 12)
-
-### 5ai_Comparing_FT_Between_Haplotypes.R
-
-Haplo_graph <- Rep_Single %>% group_by(Generation, Haplotype) %>% summarise(Avg_FT = mean(FT)) %>% ungroup()
-Haplo_graph <- Haplo_graph %>% group_by(Generation) %>% mutate(GenAvg = mean(Avg_FT)) %>% ungroup()
-Haplo_graph$`Avg FT > Pop & Gen Avg FT` <- ifelse(Haplo_graph$Avg_FT > Haplo_graph$GenAvg & Haplo_graph$Avg_FT > mean(Haplo_graph$Avg_FT), TRUE, FALSE)
-f_FT <- Haplo_graph %>% filter(`Avg FT > Pop & Gen Avg FT` == TRUE) %>% filter(!duplicated(Haplotype)) %>% select(Haplotype)
-
-ggplot(Haplo_graph,aes(x= Haplotype, y = Avg_FT, fill = `Avg FT > Pop & Gen Avg FT`)) +
-  geom_bar(stat = "identity") +
-  geom_hline(yintercept = mean(Haplo_graph$Avg_FT), color = "green") +
-  geom_hline(aes(yintercept = GenAvg), color = "blue", linetype = 2) +
-  labs(y = "Average Flowering Time (Days)",
-       title = "Average Flowering Time between Haplotypes") +
-  facet_wrap(~Generation, scales = "free_x") + 
-  theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(values = c("black", "red"))
-ggsave("scripts/plotting/05ai_Average_FT_Haplotypes_2022_2023.png", width = 16, height = 12)
-
-### 5aii_Comparing_Fecundity_Between_Haplotypes.R
-
-Haplo_graph <- Rep_Single %>% group_by(Generation, Haplotype) %>% summarise(Avg_Fec = mean(Fecundity)) %>% ungroup()
-Haplo_graph <- Haplo_graph %>% group_by(Generation) %>% mutate(GenAvg = mean(Avg_Fec)) %>% ungroup()
-Haplo_graph$`Avg Fec > Pop & Gen Avg Fec` <- ifelse(Haplo_graph$Avg_Fec > Haplo_graph$GenAvg & Haplo_graph$Avg_Fec > mean(Haplo_graph$Avg_Fec), TRUE, FALSE)
-f_Fe <- Haplo_graph %>% filter(`Avg Fec > Pop & Gen Avg Fec` == TRUE) %>% filter(!duplicated(Haplotype)) %>% select(Haplotype)
-
-ggplot(Haplo_graph,aes(x= Haplotype, y = Avg_Fec, fill = `Avg Fec > Pop & Gen Avg Fec`)) +
-  geom_bar(stat = "identity") +
-  geom_hline(yintercept = mean(Haplo_graph$Avg_Fec), color = "green") +
-  geom_hline(aes(yintercept = GenAvg), color = "blue", linetype = 2) +
-  labs(y = "Average Fecundity",
-       title = "Average Fecundity between Haplotypes") +
-  facet_wrap(~Generation, scales = "free_x") + 
-  theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(values = c("black", "red"))
-ggsave("scripts/plotting/05aii_Average_Fec_Haplotypes_2022_2023.png", width = 16, height = 12)
-
-### 5aiii_Comparing_Fitness_Between_Haplotypes.R
-
-Haplo_graph <- Rep_Single %>% group_by(Generation, Haplotype) %>% summarise(Avg_Fit = mean(Fitness)) %>% ungroup()
-Haplo_graph <- Haplo_graph %>% group_by(Generation) %>% mutate(GenAvg = mean(Avg_Fit)) %>% ungroup()
-Haplo_graph$`Avg Fit > Pop & Gen Avg Fit` <- ifelse(Haplo_graph$Avg_Fit > Haplo_graph$GenAvg & Haplo_graph$Avg_Fit > mean(Haplo_graph$Avg_Fit), TRUE, FALSE)
-f_Fi <- Haplo_graph %>% filter(`Avg Fit > Pop & Gen Avg Fit` == TRUE) %>% filter(!duplicated(Haplotype)) %>% select(Haplotype)
-ui <- Reduce(intersect, list(f_AY, f_Fe, f_Fi))
-  
-ggplot(Haplo_graph,aes(x= Haplotype, y = Avg_Fit, fill = `Avg Fit > Pop & Gen Avg Fit`)) +
-  geom_bar(stat = "identity") +
-  geom_hline(yintercept = mean(Haplo_graph$Avg_Fit), color = "green") +
-  geom_hline(aes(yintercept = GenAvg), color = "blue", linetype = 2) +
-  labs(y = "Average Fitness",
-       title = "Average Fitness between Haplotypes") +
-  facet_wrap(~Generation, scales = "free_x") + 
-  theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(values = c("black", "red"))
-ggsave("scripts/plotting/05aiii_Average_Fit_Haplotypes_2022_2023.png", width = 16, height = 12)
+source("../scripts/CUSTOM_FNS.R")
+df <- read_delim("weighted_generation_trait_avgs.tsv")
 
 
-  
-for (i in unique(T$Generation)){
-  tmp <-  ggplot(subset(T, Generation == i), aes(Genotype, TW)) + geom_bar(stat = "identity") + 
-    theme(axis.text.x = element_text(angle = 60)) + labs(y = "Total Weight (g)") +
-    ggtitle(paste0("Generation ", i))
-  ggsave(tmp, file = paste0("Bar_Plot_Total_Weight_Generation_", i, ".png"))
-}
+## plot weighted trait averages over generations
+
+# format data longways
+df_lng <- df %>% pivot_longer(cols=colnames(df)[2:7], names_to="trait", values_to="value")
+df_lng$trait <- tidy_text_substitution(df_lng$trait)
+
+
+g <- ggplot(df_lng, aes(x= generation, y = value)) +
+      geom_point() + geom_line() +
+      facet_wrap(~trait, scales="free_y") + 
+      labs(title = "Trait Average over Generations", subtitle="Haplotype-Frequency Weighted", x="Generation", y="") +
+      theme_bw(base_size = 16) 
+ggsave("../results/trait_avg_over_gens_hap_weighted.png", g, width = 16, height = 12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Atlas
+
+Average_Haplo_rep <- Average_Haplo_rep %>%  mutate(Atlas_Avg_Fec = 2363.51,
+                  Atlas_Avg_Fitness = 21347.22,
+                  Atlas_Avg_Total_Weight = 126.8267)
+Average_Haplo_rep$Numbers <- ifelse(Average_Haplo_rep$Condition == "mixed", 1, 0)
+
+
+### Adding Averaged Atlas values into the table and adding columns for centered data
+
+Atlas_tbl <- sw_avg %>% filter(Genotype == "48_5") %>%
+  mutate(Atlas_Avg_Fec = mean(FECUNDITY),
+         Atlas_Avg_Fit = mean(FITNESS),
+         Atlas_Avg_TW = mean(TOTAL_MASS))
+
+sw_avg <- sw_avg %>% mutate(Atlas_Avg_Fec = 1413.799,
+            Atlas_Avg_Fit = 12880.63,
+            Atlas_Avg_TW = 64.55571)
+
+sw_avg <- sw_avg %>% mutate(Centered_Fit = FITNESS - mean(FITNESS, na.rm = TRUE),
+Centered_FT = FT - mean(FT, na.rm = TRUE),
+Centered_Fec = FECUNDITY - mean(FECUNDITY, na.rm = TRUE),
+Centered_TW = TOTAL_MASS - mean(TOTAL_MASS))
+
+### Calculating Contribution of each seed to phenotype
+
+### FECUNDITY
+sw_avg$Exp_Fec_Per_Plant <- ifelse(sw_avg$Condition == "mixed",
+Exp_Fec_Mixed(sw_avg$FEC),
+Exp_Single(sw_avg$FEC))
+
+### FITNESS
+sw_avg$Exp_Fit_Per_Plant <- ifelse(sw_avg$Condition == "mixed",
+Exp_Fit_Mixed(sw_avg$FITNESS),
+Exp_Single(sw_avg$FITNESS))
+
+### Total Weight
+sw_avg$Exp_TW_Per_Plant <- ifelse(sw_avg$Condition == "mixed",
+Exp_TW_mix(sw_avg$TOTAL_MASS),
+Exp_Single(sw_avg$TOTAL_MASS))
