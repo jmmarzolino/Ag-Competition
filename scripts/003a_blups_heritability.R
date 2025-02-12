9d55865ade5856ae43a14e008495da2375b5b145
Jill Marzolino <jmarz001@ucr.edu>
2025-02-11 22:50:04 -0800

    update traits and models

--- scripts/003a_blups_heritability.R
+++ scripts/003a_blups_heritability.R
@@ -14,10 +14,9 @@ source("../scripts/CUSTOM_FNS.R")
 # Loading Data
 pheno <- read_delim("DERIVED_PHENOTYPES.tsv")
 
-pheno <- pheno %>% 
-    filter(Condition == "single") %>% 
-    select(c(Genotype, Generation, Replicate, Exp_year, FT, TOTAL_MASS, GERMINATION, SEED_WEIGHT_100, FECUNDITY, FITNESS)) #%>% 
-    #mutate(across(-c(Genotype, Generation, Replicate, GERMINATION), ~(scale(.) %>% as.vector)))
+#pheno <- pheno %>% 
+#    select(c(Genotype, Replicate, Exp_year, FT, TOTAL_MASS, SEED_WEIGHT_100, FECUNDITY)) #%>% 
+    #mutate(across(-c(Genotype, Generation, Replicate, Plants), ~(scale(.) %>% as.vector)))
 
 
 ## extract blups & H2 for each trait & model
@@ -33,8 +32,8 @@ H2cal(data = pheno
           , rep.n = 2
           , year.n = 2
           , year.name = "Exp_year"
-          , fixed.model = "0 + Exp_year + Genotype + (1|Genotype:Exp_year)"
-          , random.model = "1 + Exp_year + (1|Genotype) + (1|Genotype:Exp_year)"
+          , fixed.model = "0 + (1|Exp_year) + Genotype + (1|Genotype:Exp_year)"
+          , random.model = "1 + (1|Exp_year) + (1|Genotype) + (1|Genotype:Exp_year)"
           , plot_diag = TRUE
           , outliers.rm = TRUE
           , summary = TRUE
@@ -46,13 +45,13 @@ H2cal(data = pheno
 #mod <-  lmer(FT ~ 1 + Exp_year + (1|Genotype), pheno)
 #pheno$resids <- resid(mod)
 #pheno$fitted <- fitted(mod)
-#ggplot(pheno, aes(fitted, resids, color=GERMINATION)) + geom_point() + geom_smooth() + geom_hline(yintercept=0)
+#ggplot(pheno, aes(fitted, resids, color=Plants)) + geom_point() + geom_smooth() + geom_hline(yintercept=0)
 
 
 ## Extract intercept + genotype data to re-scale data to original units
 ### trait average + genotypes deviance from average
-inter <- summary(h2_ft$model)$coefficients[1, 1]
-h2_ft$blups$FT <- h2_ft$blups$FT + inter
+#inter <- summary(h2_ft$model)$coefficients[1, 1]
+#h2_ft$blups$FT <- h2_ft$blups$FT
 
 
 
@@ -64,21 +63,37 @@ H2cal(data = pheno
           , rep.n = 2
           , year.n = 2
           , year.name = "Exp_year"
-          , fixed.model = "0 + GERMINATION + (1|Replicate) + Genotype + (1|Genotype:Exp_year)"
-          , random.model = "1 + GERMINATION + (1|Replicate)  + (1|Genotype) + (1|Genotype:Exp_year)"
+          , fixed.model = "0 + (1|Exp_year) + Plants + (1|Replicate) + Genotype + (1|Genotype:Exp_year)"
+          , random.model = "1 + (1|Exp_year) + Plants + (1|Replicate)  + (1|Genotype) + (1|Genotype:Exp_year)"
           , plot_diag = TRUE
           , outliers.rm = TRUE
           , summary = TRUE
           )
 
 
-#lmer(TOTAL_MASS ~ 1 + Exp_year + GERMINATION + Replicate + (1|Genotype) + (1|Genotype:Exp_year), pheno) 
+#lmer(TOTAL_MASS ~ 1 + Exp_year + Plants + Replicate + (1|Genotype) + (1|Genotype:Exp_year), pheno) 
 
 #lmer(TOTAL_MASS ~ 1 + Exp_year + FITNESS + Generation + Replicate + (1|Genotype) + (1|Genotype:Exp_year), pheno)
 
-inter <- summary(h2_totmass$model)$coefficients[1, 1]
-h2_totmass$blups$TOTAL_MASS <- h2_totmass$blups$TOTAL_MASS + inter
 
+h2_massper <- 
+H2cal(data = pheno
+          , trait = "MASS_PER_PLANT"
+          , gen.name = "Genotype"
+          , rep.n = 2
+          , year.n = 2
+          , year.name = "Exp_year"
+          , fixed.model = "0 + (1|Exp_year) + Plants + (1|Replicate) + Genotype + (1|Genotype:Exp_year)"
+          , random.model = "1 + (1|Exp_year) + Plants + (1|Replicate)  + (1|Genotype) + (1|Genotype:Exp_year)"
+          , plot_diag = TRUE
+          , outliers.rm = TRUE
+          , summary = TRUE
+          )
+
+
+#inter <- summary(h2_totmass$model)$coefficients[1, 1]
+#h2_totmass$blups$TOTAL_MASS <- h2_totmass$blups$TOTAL_MASS
+# interesting note in mass per plant - modeling with 'Plants' shows negative relationship btwn mass per plant and number of plants
 
 
 h2_sw100 <- 
@@ -88,23 +103,23 @@ H2cal(data = pheno
           , rep.n = 2
           , year.n = 2
           , year.name = "Exp_year"
-          , fixed.model = "0 + Genotype + (1|Genotype:Exp_year)"
-          , random.model = "1 + (1|Genotype) + (1|Genotype:Exp_year)"
+          , fixed.model = "0 + TOTAL_MASS + (1|Exp_year) + Genotype + FT + (1|Genotype:Exp_year)"
+          , random.model = "1 + TOTAL_MASS + (1|Exp_year) + (1|Genotype) + FT + (1|Genotype:Exp_year)"
           , plot_diag = TRUE
           , outliers.rm = TRUE
           , summary = TRUE
           )
 
-inter <- summary(h2_sw100$model)$coefficients[1, 1]
-h2_sw100$blups$SEED_WEIGHT_100 <- h2_sw100$blups$SEED_WEIGHT_100 + inter
+#inter <- summary(h2_sw100$model)$coefficients[1, 1]
+#h2_sw100$blups$SEED_WEIGHT_100 <- h2_sw100$blups$SEED_WEIGHT_100 + inter
 
 
 
 
 ## not fitable
-h2_germ <- 
+h2_plants <- 
 H2cal(data = pheno
-        , trait = "GERMINATION"
+        , trait = "Plants"
         , gen.name = "Genotype"
         , rep.n = 2
         , year.n = 2
@@ -115,8 +130,8 @@ H2cal(data = pheno
         , outliers.rm = TRUE
         )
 
-inter <- summary(h2_germ$model)$coefficients[1, 1]
-h2_germ$blups$GERMINATION <- h2_germ$blups$GERMINATION + inter
+#inter <- summary(h2_plants$model)$coefficients[1, 1]
+#h2_plants$blups$Plants <- h2_plants$blups$Plants + inter
 
 
 
@@ -127,46 +142,26 @@ H2cal(data = pheno
           , rep.n = 2
           , year.n = 2
           , year.name = "Exp_year"
-          , fixed.model = "0 + GERMINATION + FT + Genotype + (1|Genotype:Exp_year)"
-          , random.model = "1 + GERMINATION + FT + (1|Genotype) + (1|Genotype:Exp_year)"
+          , fixed.model = "0 + Plants + FT + Genotype + (1|Genotype:Exp_year)"
+          , random.model = "1 + Plants + FT + (1|Genotype) + (1|Genotype:Exp_year)"
           , plot_diag = TRUE
           , outliers.rm = TRUE
           )
 
-#mod <- lmer(FECUNDITY ~ 1 + GERMINATION + FT + (1|Genotype) + (1|Genotype:Exp_year), pheno)
+#mod <- lmer(FECUNDITY ~ 1 + Plants + FT + (1|Genotype) + (1|Genotype:Exp_year), pheno)
 #pheno$resids <- resid(mod)
 #pheno$fits <- fitted(mod)
 
 #ggplot(pheno, aes(fitted, resids)) + geom_point() + geom_smooth() + geom_hline(yintercept=0)
 
-inter <- summary(h2_fec$model)$coefficients[1, 1]
-h2_fec$blups$FECUNDITY <- h2_fec$blups$FECUNDITY + inter
-
-
-
-
-h2_fit <- 
-H2cal(data = pheno
-        , trait = "FITNESS"
-        , gen.name = "Genotype"
-        , rep.n = 2
-        , year.n = 2
-        , year.name = "Exp_year"
-        , fixed.model = "0 + FT + Genotype + (1|Genotype:Exp_year)"
-        , random.model = "1 + FT + (1|Genotype) + (1|Genotype:Exp_year)"
-        , plot_diag = TRUE
-        , outliers.rm = TRUE
-        )
-
-inter <- summary(h2_fit$model)$coefficients[1, 1]
-h2_fit$blups$FITNESS <- h2_fit$blups$FITNESS + inter
-
+#inter <- summary(h2_fec$model)$coefficients[1, 1]
+#h2_fec$blups$FECUNDITY <- h2_fec$blups$FECUNDITY + inter
 
 dev.off()
 
 
 H2_table <- tibble("trait" = colnames(pheno)[5:ncol(pheno)], 
-                    "H2_s" = c(h2_ft$tabsmr$H2.s, h2_totmass$tabsmr$H2.s, h2_sw100$tabsmr$H2.s, h2_germ$tabsmr$H2.s, h2_fec$tabsmr$H2.s, h2_fit$tabsmr$H2.s) )
+                    "H2_s" = c(h2_ft$tabsmr$H2.s, h2_totmass$tabsmr$H2.s, h2_sw100$tabsmr$H2.s, h2_plants$tabsmr$H2.s, h2_fec$tabsmr$H2.s) )
 write_delim(H2_table, "trait_heritability.tsv", "\t")
 
 
@@ -174,7 +169,7 @@ write_delim(H2_table, "trait_heritability.tsv", "\t")
 # combine BLUP dataframes
 blup_output <- full_join(h2_ft$blups, h2_totmass$blups)
 blup_output <- full_join(blup_output, h2_sw100$blups)
-blup_output <- full_join(blup_output, h2_germ$blups)
+blup_output <- full_join(blup_output, h2_plants$blups)
 blup_output <- full_join(blup_output, h2_fec$blups)
 blup_output <- full_join(blup_output, h2_fit$blups)
 
