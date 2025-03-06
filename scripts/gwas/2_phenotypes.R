1f9a49b153963832b423489b561fbe5201178534
Jill Marzolino <jmarz001@ucr.edu>
2025-03-06 10:10:46 -0800

    clean up text & swap inner for left join

--- scripts/gwas/2_phenotypes.R
+++ scripts/gwas/2_phenotypes.R
@@ -11,13 +11,14 @@ library(data.table)
 setwd("/rhome/jmarz001/bigdata/Ag-Competition/results/gwas")
 source("../../scripts/CUSTOM_FNS.R")
 # read in phenotypes file
-#pheno <- fread("../../data/FITNESS.tsv")
 pheno <- fread("../../data/trait_BLUPs.tsv")
 
 ## filter trait cols with 0 variance
-novar <- which(apply(pheno[2:ncol(pheno)], 2, var) == 0)
-pheno <- pheno %>% select(-c(names(novar)))
+#novar <- which(apply(pheno[2:ncol(pheno)], 2, var) == 0)
+#pheno <- pheno %>% select(-c(names(novar)))
 
+# shorten genotype codes to 2 generations (\\d_\\d), for consistency, IDing genotype families instead of lines
+pheno$Genotype <- gsub("(\\d+_\\d+)_\\d+", "\\1", pheno$Genotype)
 # remove parent lines that won't be in gwas
 pheno <- add_generation(pheno)
 pheno <- pheno %>% filter(Generation != 0) %>% select(-Generation)
@@ -41,7 +42,13 @@ plink_file <- plink_file[,1:5]
 
 # find union of genotype numbers between the two data sets
 # & keep only overlapping genotypes
-joined <- inner_join(plink_file, pheno, by=c('V2'='Genotype'))
+joined <- left_join(plink_file, pheno, by=c('V2'='Genotype'))
+
+# are fam & vcf genotypes in the same order?
+#plink_file$V1 == pheno$Genotype
+#plink_file$V1 == joined$V1
+#plink_file$V1 == joined$V2
+#joined[which(!(joined$V2 %in% plink_file$V2))]
 
 ### over-write formatted genotype and phenotype .fam file
 ## format = famID / indvID / <phenotype / cols /...>
