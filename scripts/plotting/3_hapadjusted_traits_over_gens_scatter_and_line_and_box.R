897898105404c9c67c78c181c62725dcdcc53d04
Jill Marzolino <jmarz001@ucr.edu>
2025-04-02 13:40:01 -0700

    update traits plotted, remove old code, slight renames

--- scripts/plotting/3_hapadjusted_traits_over_gens_scatter_and_line_and_box.R
+++ scripts/plotting/3_hapadjusted_traits_over_gens_scatter_and_line_and_box.R
@@ -7,19 +7,15 @@
 #SBATCH -p koeniglab
 
 library(tidyverse)
+library(ggpubr)
+library(RColorBrewer)
 
 setwd("/bigdata/koeniglab/jmarz001/Ag-Competition/data")
 source("../scripts/CUSTOM_FNS.R")
 df <- read_delim("trait_BLUPs_HapRepPop.tsv")
 
 
-
-
-
-# This script plots scatterplots with linear regressions for each trait in the single subpopulation, as well as distributions for these traits
-
-
-
+### plot scatterplots with linear regressions and distributions for each trait
 
 # add column for parent or progeny marker
 df$pgroup <- "Parents"
@@ -31,72 +27,48 @@ df[which(df$Generation != 0), which(colnames(df) == "pgroup")] <- "Progeny"
 adjusted_blues <- brewer.pal(7, "Blues")[3:7]
 
 
-
-## boxplots comparing traits over generations
-#a <- ggplot(df_long, aes(y=VALUE, x=Generation, group=Generation)) +
-#      facet_wrap(~TRAIT, scales="free") +
-#      geom_boxplot(width=0.5, outliers=F) +
-#      geom_jitter(width=0.2) +
-#      theme_bw(base_size=20) +
-#      labs(x = "Generation", y = "") 
-#ggsave("results/traits_over_generations_scatterplot.png", a, width = 14, height = 10)
-
-# when using boxplot & jitter ....      
-# some points are doubled b/c they're plotted as part of jitter & boxplot-outliers
-# you can color boxplot outliers, or not include them (outliers=T/F, outlier.color/shape/size/alpha...)
-
-
-# dot plot with generation-fit line
-#t <- ggplot(df_long, aes(y=VALUE, x=Generation)) +
-#      geom_jitter() +
-#      geom_smooth(method="lm") + stat_regline_equation() +
-#      theme_bw(base_size=20) +
-#      facet_wrap(~TRAIT, scales="free")+
-#      labs(x = "Generation", y = "") 
-#ggsave("results/traits_over_generations_scatterplot_w_trendline.png", t)
-
-
-
-
-
-# line for generation means...
+# calculate generation means to use as gen-avg line points
 gen_men <- df %>%
             group_by(Generation) %>%
-            summarise(across(where(is.numeric), mean)) 
+            summarise(across(where(is.numeric), \(x) mean(x, na.rm=T))) 
 
-### strip plots
+### make strip plots
 a <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=FT)) + 
       geom_line(data=gen_men, aes(x=Generation, y=FT), color="blue", linewidth=1) + 
       theme_bw(base_size=18) +
-      labs(x = "Generation", y = tidy_text_substitution("FT"), title="Flowering Time over Generations") #, subtitle="Line connects generation averages") 
+      labs(x = "Generation", y = tidy_text_substitution("FT"), title="Flowering Time over Generations") 
 
-# scale total mass by a factor of 10^10
-tmp <- df %>% mutate('TOTAL_MASS_scaled' = (TOTAL_MASS + 0.5484163055) * 10^11)
-mean_tmp <- tmp %>% group_by(Generation) %>% summarise('totmas' = mean(TOTAL_MASS_scaled)) 
-b <- ggplot(tmp) + 
-      geom_jitter(aes(x=Generation, y=TOTAL_MASS_scaled)) +
-      geom_line(data=mean_tmp, aes(x=Generation, y=totmas), color="blue", linewidth=1) + 
+b <- ggplot() + 
+      geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=TOTAL_MASS)) + 
+      geom_line(data=gen_men, aes(x=Generation, y=TOTAL_MASS), color="blue", linewidth=1) + 
       theme_bw(base_size=18) +
       labs(x = "Generation", y = tidy_text_substitution("TOTAL_MASS"), title="Total Seed Weight over Generations") 
 
 c <- ggplot() + 
-      geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=Plants)) + 
-      geom_line(data=gen_men, aes(x=Generation, y=Plants), color="blue", linewidth=1) + 
+      geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=SEED_WEIGHT_100)) + 
+      geom_line(data=gen_men, aes(x=Generation, y=SEED_WEIGHT_100), color="blue", linewidth=1) + 
       theme_bw(base_size=18) +
-      labs(x = "Generation", y = tidy_text_substitution("Plants"), title="Seed Germination over Generations") 
+      labs(x = "Generation", y = tidy_text_substitution("SEED_WEIGHT_100"), title="Genotype 100-Seed Weight over Generations") 
 
 d <- ggplot() + 
-      geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=FECUNDITY)) + 
-      geom_line(data=gen_men, aes(x=Generation, y=FECUNDITY), color="blue", linewidth=1) + 
-      theme_bw(base_size=18) +
-      labs(x = "Generation", y = tidy_text_substitution("FECUNDITY"), title="Genotype Fecundity over Generations") 
-
-e <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=MASS_PER_PLANT)) + 
       geom_line(data=gen_men, aes(x=Generation, y=MASS_PER_PLANT), color="blue", linewidth=1) + 
       theme_bw(base_size=18) +
       labs(x = "Generation", y = tidy_text_substitution("MASS_PER_PLANT"), title="Genotype Mass per Plant over Generations") 
 
-ggcombo <- ggarrange(a, b, c, d, e)
-ggsave("results/traits_over_generations_scatterplots.png", ggcombo, width = 20, height = 10)
+e <- ggplot() + 
+      geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=Plants)) + 
+      geom_line(data=gen_men, aes(x=Generation, y=Plants), color="blue", linewidth=1) + 
+      theme_bw(base_size=18) +
+      labs(x = "Generation", y = tidy_text_substitution("Plants"), title="Seed Germination over Generations") 
+
+f <- ggplot() + 
+      geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=FECUNDITY)) + 
+      geom_line(data=gen_men, aes(x=Generation, y=FECUNDITY), color="blue", linewidth=1) + 
+      theme_bw(base_size=18) +
+      labs(x = "Generation", y = tidy_text_substitution("FECUNDITY"), title="Genotype Fecundity over Generations") 
+
+# combine and save plots
+ggcombo <- ggarrange(a, b, c, d, e, f)
+ggsave("../results/traits_over_generations_scatterplots_hap_weighted.png", ggcombo, width = 20, height = 10)
