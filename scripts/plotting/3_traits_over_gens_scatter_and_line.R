4851551c5cc327b1b4b5e321f776b840d43aa1df
Jill Marzolino <jmarz001@ucr.edu>
2025-04-08 18:12:37 -0700

    update plotting to include boxplots, new colors, spacing

--- scripts/plotting/3_traits_over_gens_scatter_and_line.R
+++ scripts/plotting/3_traits_over_gens_scatter_and_line.R
@@ -24,40 +24,43 @@ df <- add_generation(df)
 # table for generation means
 gen_men <- df %>%
             group_by(Generation) %>%
-            summarise(across(where(is.numeric), mean)) 
+            summarise(across(where(is.numeric), \(x) mean(x, na.rm=T))) 
 
-### strip plots
+
+## strip plots for each trait
 a <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=FT)) + 
-      geom_line(data=gen_men, aes(x=Generation, y=FT), color="blue", linewidth=1) + 
-      theme_bw(base_size=18) +
-      labs(x = "Generation", y = tidy_text_substitution("FT"), title="Flowering Time over Generations") #, subtitle="Line connects generation averages") 
+      geom_boxplot(data=df, aes(x=Generation, group=as.factor(Generation), y=FT), outliers=F, fill = NA) +
+      geom_line(data=gen_men, aes(x=Generation, y=FT), color="dodgerblue3", linewidth=1) + 
+      theme_bw(base_size=16) +
+      labs(x = "Generation", y = tidy_text_substitution("FT"), title="Flowering Time over Generations") 
 
-# scale total mass by a factor of 10^10
-tmp <- df %>% mutate('TOTAL_MASS_scaled' = (TOTAL_MASS + 0.5484163055) * 10^11)
-mean_tmp <- tmp %>% group_by(Generation) %>% summarise('totmas' = mean(TOTAL_MASS_scaled)) 
-b <- ggplot(tmp) + 
-      geom_jitter(aes(x=Generation, y=TOTAL_MASS_scaled)) +
-      geom_line(data=mean_tmp, aes(x=Generation, y=totmas), color="blue", linewidth=1) + 
-      theme_bw(base_size=18) +
+b <- ggplot(df) + 
+      geom_jitter(aes(x=Generation, y=TOTAL_MASS)) +
+      geom_boxplot(data=df, aes(x=Generation, group=as.factor(Generation), y=TOTAL_MASS), outliers=F, fill = NA) +
+      geom_line(data=gen_men, aes(x=Generation, y=TOTAL_MASS), color="dodgerblue3", linewidth=1) + 
+      theme_bw(base_size=16) +
       labs(x = "Generation", y = tidy_text_substitution("TOTAL_MASS"), title="Total Seed Weight over Generations") 
 
 c <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=Plants)) + 
-      geom_line(data=gen_men, aes(x=Generation, y=Plants), color="blue", linewidth=1) + 
-      theme_bw(base_size=18) +
+      geom_boxplot(data=df, aes(x=Generation, group=as.factor(Generation), y=Plants), outliers=F, fill = NA) +
+      geom_line(data=gen_men, aes(x=Generation, y=Plants), color="dodgerblue3", linewidth=1) + 
+      theme_bw(base_size=16) +
       labs(x = "Generation", y = tidy_text_substitution("Plants"), title="Seed Germination over Generations") 
 
 d <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=FECUNDITY)) + 
-      geom_line(data=gen_men, aes(x=Generation, y=FECUNDITY), color="blue", linewidth=1) + 
-      theme_bw(base_size=18) +
+      geom_boxplot(data=df, aes(x=Generation, group=as.factor(Generation), y=FECUNDITY), outliers=F, fill = NA) +
+      geom_line(data=gen_men, aes(x=Generation, y=FECUNDITY), color="dodgerblue3", linewidth=1) + 
+      theme_bw(base_size=16) +
       labs(x = "Generation", y = tidy_text_substitution("FECUNDITY"), title="Genotype Fecundity over Generations") 
 
 e <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation, group=as.factor(Generation), y=MASS_PER_PLANT)) + 
-      geom_line(data=gen_men, aes(x=Generation, y=MASS_PER_PLANT), color="blue", linewidth=1) + 
-      theme_bw(base_size=18) +
+      geom_boxplot(data=df, aes(x=Generation, group=as.factor(Generation), y=MASS_PER_PLANT), outliers=F, fill = NA) +
+      geom_line(data=gen_men, aes(x=Generation, y=MASS_PER_PLANT), color="dodgerblue3", linewidth=1) + 
+      theme_bw(base_size=16) +
       labs(x = "Generation", y = tidy_text_substitution("MASS_PER_PLANT"), title="Genotype Mass per Plant over Generations") 
 
 ggcombo <- ggarrange(a, b, c, d, e)
