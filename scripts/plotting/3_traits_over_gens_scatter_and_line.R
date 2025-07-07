29bb81935bff7ba6d4cd5d23e81f1cc1e4f0003f
Jill Marzolino <jmarz001@ucr.edu>
2025-07-07 12:55:10 -0700

    remove haplotype average line from scatterplots

--- scripts/plotting/3_traits_over_gens_scatter_and_line.R
+++ scripts/plotting/3_traits_over_gens_scatter_and_line.R
@@ -18,68 +18,49 @@ df <- read_delim("data/trait_BLUPs.tsv")
 df <- add_generation(df)
 
 #comparing trait values over generations
-## scatter- & box- plots 
-
+## scatter plots 
 
 # table for generation means
 gen_men <- df %>%
             group_by(Generation) %>%
             summarise(across(where(is.numeric), \(x) mean(x, na.rm=T))) 
-gen_men$experiment <- "observed"
-
-# import haplotype data & average by generation
-hap <- read_delim("data/trait_BLUPs_HapRepPop.tsv")
-hap_gen_men <- hap %>%
-            group_by(Generation) %>%
-            summarise(across(where(is.numeric), \(x) mean(x, na.rm=T))) 
-hap_gen_men$experiment <- "simulated"
-
-gen_means <- full_join(gen_men, hap_gen_men)
 
 ## strip plots for each trait
 a <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation, y=FT), alpha=0.7) + 
-      geom_line(data=gen_means, aes(x=Generation, y=FT, color=experiment), linewidth=1) + 
+      geom_line(data=gen_men, aes(x=Generation, y=FT), color="darkblue", linewidth=1) + 
       theme_bw(base_size=16) +
-      labs(x = "Generation", y = tidy_text_substitution("FT"), title="Flowering Time over Generations") +
-      scale_color_manual(values=c("darkblue", "dodgerblue3"), name="averaged data")
+      labs(x = "Generation", y = tidy_text_substitution("FT"), title="Flowering Time over Generations") 
 
 b <- ggplot(df) + 
       geom_jitter(aes(x=Generation, y=TOTAL_MASS), alpha=0.7) +
-      geom_line(data=gen_means, aes(x=Generation, y=TOTAL_MASS, color=experiment), linewidth=1) + 
+      geom_line(data=gen_men, aes(x=Generation, y=TOTAL_MASS), color="darkblue", linewidth=1) + 
       theme_bw(base_size=16) +
-      labs(x = "Generation", y = tidy_text_substitution("TOTAL_MASS"), title="Total Seed Weight over Generations") +
-      scale_color_manual(values=c("darkblue", "dodgerblue3"), name="averaged data")
+      labs(x = "Generation", y = tidy_text_substitution("TOTAL_MASS"), title="Total Seed Weight over Generations") 
 
 c <- ggplot(df) + 
       geom_jitter(aes(x=Generation, y=SEED_WEIGHT_100), alpha=0.7) +
-      geom_line(data=gen_means, aes(x=Generation, y=SEED_WEIGHT_100, color=experiment), linewidth=1) + 
+      geom_line(data=gen_men, aes(x=Generation, y=SEED_WEIGHT_100), color="darkblue", linewidth=1) + 
       theme_bw(base_size=16) +
-      labs(x = "Generation", y = tidy_text_substitution("SEED_WEIGHT_100"), title="Mass per 100-seeds over Generations") +
-      scale_color_manual(values=c("darkblue", "dodgerblue3"), name="averaged data")
+      labs(x = "Generation", y = tidy_text_substitution("SEED_WEIGHT_100"), title="Mass per 100-seeds over Generations") 
 
 d <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation, y=MASS_PER_PLANT), alpha=0.7) + 
-      geom_line(data=gen_means, aes(x=Generation, y=MASS_PER_PLANT, color=experiment), linewidth=1) + 
+      geom_line(data=gen_men, aes(x=Generation, y=MASS_PER_PLANT), color="darkblue", linewidth=1) + 
       theme_bw(base_size=16) +
-      labs(x = "Generation", y = tidy_text_substitution("MASS_PER_PLANT"), title="Genotype Mass per Plant over Generations") +
-      scale_color_manual(values=c("darkblue", "dodgerblue3"), name="averaged data")
+      labs(x = "Generation", y = tidy_text_substitution("MASS_PER_PLANT"), title="Genotype Mass per Plant over Generations") 
 
 e <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation, y=Germination), alpha=0.7) + 
-      geom_line(data=gen_means, aes(x=Generation, y=Germination, color=experiment), linewidth=1) + 
+      geom_line(data=gen_men, aes(x=Generation, y=Germination), color="darkblue", linewidth=1) + 
       theme_bw(base_size=16) +
-      labs(x = "Generation", y = tidy_text_substitution("Germination"), title="Seed Germination over Generations") +
-      scale_color_manual(values=c("darkblue", "dodgerblue3"), name="averaged data")
+      labs(x = "Generation", y = tidy_text_substitution("Germination"), title="Seed Germination over Generations") 
 
 f <- ggplot() + 
       geom_jitter(data=df, aes(x=Generation,y=FECUNDITY), alpha=0.7) + 
-      geom_line(data=gen_means, aes(x=Generation, y=FECUNDITY, color=experiment), linewidth=1) + 
+      geom_line(data=gen_men, aes(x=Generation, y=FECUNDITY), color="darkblue", linewidth=1) + 
       theme_bw(base_size=16) +
-      labs(x = "Generation", y = tidy_text_substitution("FECUNDITY"), title="Genotype Fecundity over Generations") +
-      scale_color_manual(values=c("darkblue", "dodgerblue3"), name="averaged data")
-
-
+      labs(x = "Generation", y = tidy_text_substitution("FECUNDITY"), title="Genotype Fecundity over Generations") 
 
 
 ggcombo <- ggarrange(a, b, c, d, e, f, ncol=3, nrow=2)
