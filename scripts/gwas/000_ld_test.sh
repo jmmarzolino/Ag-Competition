887dbca371ca441e2c7c6dba4d7cbcaa244d19e6
Jill Marzolino <jmarz001@ucr.edu>
2025-07-15 18:14:51 -0700

    make script array to run over each chr in turn

--- scripts/gwas/000_ld_test.sh
+++ scripts/gwas/000_ld_test.sh
@@ -5,53 +5,24 @@
 #SBATCH --mem=100G
 #SBATCH -p koeniglab
 
-# GEMMA 0.98
 cd /rhome/jmarz001/bigdata/Ag-Competition/results/gwas
 
 module load plink/1.90b6.25
 
+CHR="chr"$SLURM_ARRAY_TASK_ID"H"
+OUT="all_traits_chr"$SLURM_ARRAY_TASK_ID
+
 plink --allow-extra-chr \
 --allow-no-sex \
 --double-id \
 --maf 0.01 \
 --keep AgComp_genotypes.tsv \
---out all_traits \
 --set-missing-var-ids @:#$1,$2 \
 --vcf combined_filt.vcf.gz \
 --r2 gz --ld-window 10 --ld-window-kb 10000 --ld-window-r2 0 \
---parallel $SLURM_ARRAY_TASK_ID 14
+--chr $CHR \
+--out $OUT
+
 #--indiv-sort f AgComp_genotypes.tsv \
 #--make-bed \
 # --distance triange
-
-## concatenate all the files
-
-
-
-
-#### zoom in on gwas results in chromosome 4 & 5 peaks for flowering time
-plink --allow-extra-chr \
---allow-no-sex \
---double-id \
---maf 0.01 \
---keep AgComp_genotypes.tsv \
---out all_traits_chr4 \
---set-missing-var-ids @:#$1,$2 \
---vcf combined_filt.vcf.gz \
---chr chr4H \
---r2 gz --ld-window 10 --ld-window-kb 10000 --ld-window-r2 0 
-
-plink --allow-extra-chr \
---allow-no-sex \
---double-id \
---maf 0.01 \
---keep AgComp_genotypes.tsv \
---out all_traits_chr5 \
---set-missing-var-ids @:#$1,$2 \
---vcf combined_filt.vcf.gz \
---chr chr5H \
---r2 gz --ld-window 10 --ld-window-kb 10000 --ld-window-r2 0 
-
-
-
-### calculate LD decay around lead snp on chr 4 & 5
