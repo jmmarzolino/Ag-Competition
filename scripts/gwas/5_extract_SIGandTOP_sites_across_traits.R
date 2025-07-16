c3cab5cad14351d602f8a747117c8cb28ba3ffe3
Jill Marzolino <jmarz001@ucr.edu>
2025-07-16 14:12:28 -0700

    swap out lm for lmm gwas model

--- scripts/gwas/5_extract_SIGandTOP_sites_across_traits.R
+++ scripts/gwas/5_extract_SIGandTOP_sites_across_traits.R
@@ -13,7 +13,7 @@ source("../../scripts/CUSTOM_FNS.R")
 
 
 file_list <- fread("association_files_traits.txt")
-files <- file_list$file
+files <- file_list$file_lmm
 
 for(i in files){
     # read in file
@@ -52,37 +52,6 @@ for(i in files){
 
 
 file_list <- fread("association_files_traits.txt")
-
-for(i in 1:nrow(file_list)){
-
-    # read in file
-    df <- fread(file_list[[i, 3]])
-
-    # extract the top x% of sites from gwas
-    # define bonnferroni threshhold for significance with multiple testing
-    bonn <- 0.05 / nrow(df)
-
-    # keep only sites at or below our threshhold
-    df <- df %>% filter(p_lrt <= bonn)
-
-    # label associated trait
-    df$associated_trait <- file_list[[i, 1]]
-
-    # now save your resulting list of sites
-    assign(gsub("(ASSOC_\\d+).assoc.txt", "\\1", file_list[[i, 3]]), df)
-}
-
-
-
-lm_files <- full_join(ASSOC_6, ASSOC_7)
-lm_files <- full_join(lm_files, ASSOC_8)
-lm_files <- full_join(lm_files, ASSOC_9)
-lm_files <- full_join(lm_files, ASSOC_10)
-lm_files <- full_join(lm_files, ASSOC_11)
-
-lm_files$gwas_method <- "lm"
-
-
 # repeat for lmm model files
 # with file name ref col changed & assigned name change
 for(i in 1:nrow(file_list)){
@@ -106,41 +75,37 @@ for(i in 1:nrow(file_list)){
 
 lmm_files <- full_join(ASSOC_6_lmm, ASSOC_7_lmm)
 lmm_files <- full_join(lmm_files, ASSOC_8_lmm)
-lmm_files <- full_join(lmm_files, ASSOC_9_lmm)
-lmm_files <- full_join(lmm_files, ASSOC_10_lmm)
-lmm_files <- full_join(lmm_files, ASSOC_11_lmm)
 
 lmm_files$gwas_method <- "lmm"
 
 
-df2 <- full_join(lm_files, lmm_files)
-df2$associated_trait <- tidy_text_substitution(df2$associated_trait)
+lmm_files$associated_trait <- tidy_text_substitution(lmm_files$associated_trait)
 
-write_delim(df2, "all_gwas_sig_sites.tsv")
+write_delim(lmm_files, "all_gwas_sig_sites.tsv")
 
 
 
 
 #### stat checks
-print(paste0("number of significant sites across traits and gwas models: ", length(df2$rs)))
-print(paste0("number of significant sites across traits: ", length(unique(df2$rs))))
+print(paste0("number of significant sites across traits and gwas models: ", length(lmm_files$rs)))
+print(paste0("number of significant sites across traits: ", length(unique(lmm_files$rs))))
 
 print("number of sig sites per trait")
-print(table(df2$associated_trait))
+print(table(lmm_files$associated_trait))
 
 print("number of significant sites for more than one trait (if any. traits w the same sig site twice indicate significance with 2 gwas models): ",)
-table(df2$rs, df2$associated_trait)[which(rowSums(table(df2$rs, df2$associated_trait)) > 1), ]
+table(lmm_files$rs, lmm_files$associated_trait)[which(rowSums(table(lmm_files$rs, lmm_files$associated_trait)) > 1), ]
 
 
 print("are there any sites overlapping trait/method?")
 print("differing numbers indicate site overlap")
-tmp <- df2 %>% select(c(chr, rs, ps, associated_trait, gwas_method))
+tmp <- lmm_files %>% select(c(chr, rs, ps, associated_trait, gwas_method))
 length(tmp$chr)
 length(unique(tmp$rs))
 
 
 print("are there any sites that are sig for multiple traits?")
 print("if numbers match, there are no overlaping sites")
-tmp2 <- df2 %>% select(c(chr, rs, ps, associated_trait)) %>% unique
+tmp2 <- lmm_files %>% select(c(chr, rs, ps, associated_trait)) %>% unique
 length(tmp2$rs)
 length(unique(tmp2$rs))
