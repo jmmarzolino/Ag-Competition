ec052a81e9672f0da5c9aaf3494c7c0180bbfdc3
Jill Marzolino <jmarz001@ucr.edu>
2025-07-17 12:49:29 -0700

    skip lm gwas

--- scripts/gwas/3_univariate_association_array.sh
+++ scripts/gwas/3_univariate_association_array.sh
@@ -25,6 +25,6 @@ PHENO_NAME="ASSOC_${COL}"
 #One can specify a different column as the phenotype column by using “-n [num]”, where ”-n 1” uses the original sixth column as phenotypes, and “-n 2” uses the seventh column, and so on and so forth.
 
 # test univariate linear model & univariate linear MIXED model
-/rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -c "$PCS" -lm 4 -n $SLURM_ARRAY_TASK_ID -outdir . -o $PHENO_NAME -debug
+#/rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -c "$PCS" -lm 4 -n $SLURM_ARRAY_TASK_ID -outdir . -o $PHENO_NAME -debug
 
 /rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -k "$KINSHIP" -c "$PCS" -lmm 4 -n $SLURM_ARRAY_TASK_ID -outdir . -o ${PHENO_NAME}_lmm -debug
