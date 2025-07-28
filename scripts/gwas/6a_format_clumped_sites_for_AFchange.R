4c08ac7e9f9b6f558116f462c4b81468298a9827
Jill Marzolino <jmarz001@ucr.edu>
2025-07-28 16:43:26 -0700

    rename for clarity of analysis timing

--- scripts/gwas/6a_format_clumped_sites_for_AFchange.R
+++ scripts/gwas/6a_format_clumped_sites_for_AFchange.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 #SBATCH --job-name=manhattan
-#SBATCH -o /rhome/jmarz001/bigdata/Ag-Competition/scripts/gwas/6_format_clumped_sites_for_AFchange.stdout
+#SBATCH -o /rhome/jmarz001/bigdata/Ag-Competition/scripts/gwas/6a_format_clumped_sites_for_AFchange.stdout
 #SBATCH --ntasks=1
 #SBATCH --mem=20gb
 #SBATCH -t 00:30:00
