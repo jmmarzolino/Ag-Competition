3bcaa1ed8afb2f1c8d5f1da63655be1c0b0fc477
Jill Marzolino <jmarz001@ucr.edu>
2025-07-30 13:36:48 -0700

    change how missing variant IDs are set & update LD calculation window params

--- scripts/gwas/6_calc_LD_for_top_sites.sh
+++ scripts/gwas/6_calc_LD_for_top_sites.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=gwas
 #SBATCH --output=/rhome/jmarz001/bigdata/Ag-Competition/scripts/gwas/6_calc_LD_for_top_sites.stdout
-#SBATCH --ntasks=14
+#SBATCH --ntasks=10
 #SBATCH --mem=100G
-#SBATCH --time=4:00:00
+#SBATCH --time=10:00:00
 #SBATCH -p koeniglab
 
 cd /rhome/jmarz001/bigdata/Ag-Competition/results/gwas
@@ -14,7 +14,14 @@ plink --allow-extra-chr \
 --allow-no-sex \
 --double-id \
 --maf 0.01 \
---set-missing-var-ids @:#$1,$2 \
+--set-missing-var-ids @:# \
 --vcf combined_filt.vcf.gz \
---r2 gz --ld-window-r2 0 --ld-window 10000 \
+--r2 gz --ld-window-r2 0 --ld-window 100 --ld-window-kb 100 \
 --out LD_10kbwin
+
+
+#--ld-snp chr1H:14499156
+#--ld-snp-list top_sites.tsv \
+#topsiteLD
+#--set-missing-var-ids @_# 
+# @==chr "_" #==position
