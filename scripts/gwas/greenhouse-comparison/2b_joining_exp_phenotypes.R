d6c08c55acec7610a122f18a3bc6872f305b8655
Jill Marzolino <jmarz001@ucr.edu>
2025-07-31 14:22:59 -0700

    clean up script purpose, number of traits, genotype filter-join, and file write out formats

--- scripts/gwas/greenhouse-comparison/2b_joining_exp_phenotypes.R
+++ scripts/gwas/greenhouse-comparison/2b_joining_exp_phenotypes.R
@@ -10,24 +10,27 @@ library(data.table)
 
 setwd("/rhome/jmarz001/bigdata/Ag-Competition/results/gwas/CCII_greenhouse_exp_gwas")
 source("../../../scripts/CUSTOM_FNS.R")
-## find the genotypes in common between Ag-Competition & CC II greenhouse experiments
+# set up phenotypes in fam file for site in common between field and greenhouse experiments
 
-# read in phenotypes file
-fld <- fread("../all_traits.fam")
-
+# read in phenotype data
 gh <- fread("CCII_Raw_phenotype_data.txt")
+# select relevant columns
+gh <- gh %>% select(c(Line, PlantHeight, Mass_100, Total_mass, Seed_Estimate, Flowering_days_2017, Flowering_2018_Median))
+# which flowering time year has more data?
+colSums(is.na(gh[,6:7]))
+
+gh <- gh %>% group_by(Line) %>% mutate("avg_FT"=mean(c(Flowering_days_2017, Flowering_2018_Median), na.rm=T)) 
+
+
 # remove parental lines
 gh <- gh[-grep("^\\d+$", gh$Line), ]
-
-# join the two trait data sets by their common genotypes
-join <- inner_join(fld, gh, by=c("V1"="Line"))
 
 # read in empty fam file & format
 fam <- fread("gh.fam")
 fam <- fam[,1:5]
 
-# check for the same genotypes
-ifelse(length(which(join$V1 != fam$V1)) != 0, "Problem with genotype match", "genotypes match")
+# attach phenotypes to fam file by joining the trait data by common genotypes
+join <- inner_join(fam, gh, by=c("V1"="Line"))
 
 # overwrite fam file w phenotype version
 write_delim(join, "gh.fam", delim=" ", col_names=F)
@@ -36,6 +39,5 @@ write_delim(join, "gh.fam", delim=" ", col_names=F)
 #trait_n <- read_delim("trait_name_to_col_numbers.tsv")
 trait_n2 <- tibble("trait_names"=colnames(gh)[2:ncol(gh)])
 trait_n2$trait_num <- (1:nrow(trait_n2))+5
-trait_n2$file <- paste0("ASSOC_", trait_n2$trait_num, ".assoc.txt")
 trait_n2$file_lmm <- paste0("ASSOC_", trait_n2$trait_num, "_lmm", ".assoc.txt")
 write_delim(trait_n2, "CCII_GH_trait_file_nums.tsv")
