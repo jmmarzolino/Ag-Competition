f7016014e2f50f18dab1dc4d0660a38a1ce32fa2
Jill Marzolino <jmarz001@ucr.edu>
2025-08-18 15:35:16 -0700

    increase job resources; streamline variables and 2 code chunks w new obj names

--- scripts/gwas/greenhouse-comparison/3_univariate_association_array.sh
+++ scripts/gwas/greenhouse-comparison/3_univariate_association_array.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=gwas
 #SBATCH --output=/rhome/jmarz001/bigdata/Ag-Competition/scripts/gwas/greenhouse-comparison/3_univariate_association_array.stdout
-#SBATCH --ntasks=2
+#SBATCH --ntasks=4
 #SBATCH --mem=80gb
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 #SBATCH -p koeniglab
 
 # GEMMA 0.98
 cd /rhome/jmarz001/bigdata/Ag-Competition/results/gwas/CCII_greenhouse_exp_gwas
 
 # define variables
-GENO=exp_common
-KINSHIP=output/related_matrix.cXX.txt
-PCS=pca.txt
 COL=$(expr $SLURM_ARRAY_TASK_ID + 5)
-PHENO_NAME="ASSOC_${COL}"
+PHENO_NAME="ASSOC_exp_common_${COL}_lmm"
+PHENO_NAME_GHONLY="ASSOC_GH_only_${COL}_lmm"
 
 ### Univariate Linear Mixed Model
 # -km 1: PLINK binary ped as relatedness matrix; 1 is default
@@ -25,10 +23,7 @@ PHENO_NAME="ASSOC_${COL}"
 #One can specify a different column as the phenotype column by using “-n [num]”, where ”-n 1” uses the original sixth column as phenotypes, and “-n 2” uses the seventh column, and so on and so forth.
 
 # univariate linear MIXED model
-/rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -k "$KINSHIP" -c "$PCS" -lmm 4 -n $SLURM_ARRAY_TASK_ID -outdir . -o ${PHENO_NAME}_lmm -debug
+/rhome/jmarz001/software/gemma0.98.5 -bfile exp_common -k output/related_matrix.cXX.txt -c exp_common_pca.txt -lmm 4 -n $SLURM_ARRAY_TASK_ID -outdir . -o "$PHENO_NAME" -debug
 
-
-## adding another quick gwas with only greenhouse results
-PHENO_NAME_GHONLY="ASSOC_GH_${COL}_lmm"
-
-/rhome/jmarz001/software/gemma0.98.5 -bfile gh_only -k output/related_matrix_gh_only.cXX.txt -c gh_only_pca.txt -lmm 4 -n $SLURM_ARRAY_TASK_ID -outdir . -o $PHENO_NAME_GHONLY -debug
+## gwas w only greenhouse results
+/rhome/jmarz001/software/gemma0.98.5 -bfile gh_only -k output/related_matrix_gh_only.cXX.txt -c gh_only_pca.txt -lmm 4 -n $SLURM_ARRAY_TASK_ID -outdir . -o "$PHENO_NAME_GHONLY" -debug
