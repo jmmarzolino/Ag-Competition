f192029aa9a54638d57d940a4d8fe290086009ce
Jill Marzolino <jmarz001@ucr.edu>
2025-09-24 14:32:45 -0700

    update trait list and kinship, pca input files

--- scripts/gwas/3_multivariate_association.sh
+++ scripts/gwas/3_multivariate_association.sh
@@ -30,6 +30,6 @@ PHENO_NAME="ASSOC_MULI"
 # restrict the number of phenotypes to be < ten
 #when a small proportion of phenotypes are partially missing, one can impute these missing values before association tests w/ -predict
 
-/rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -lmm 4 -n 1 2 3 4 5 6 -outdir . -o $PHENO_NAME -debug
-#-k "$KINSHIP" 
-#-c "$PCS"
+/rhome/jmarz001/software/gemma0.98.5 -bfile "$GENO" -lmm 4 -n 1 2 3 -k "$KINSHIP" -c "$PCS" -outdir . -o $PHENO_NAME -debug
+#
+#
