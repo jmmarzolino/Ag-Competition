3ca4b0daf57eb2cc0f16738bf6934708573b0678
Jill Marzolino <jmarz001@ucr.edu>
2025-11-08 12:23:05 -0800

    adjust trait text

--- scripts/002c_derived_data_replicate_correlations.R
+++ scripts/002c_derived_data_replicate_correlations.R
@@ -10,11 +10,11 @@ library(data.table)
 setwd("/rhome/jmarz001/bigdata/Ag-Competition/data")
 source("../scripts/CUSTOM_FNS.R")
 # load data
-df <- fread("DERIVED_PHENOTYPES_FULL.tsv")
+df <- fread("DERIVED_PHENOTYPES.tsv")
 
 
 replicate_df <- df %>%
-  pivot_longer(cols=c(FT, TOTAL_MASS, SEED_WEIGHT_100, Germination, SEED_COUNT, FECUNDITY, MASS_PER_PLANT), names_to='PHENOTYPE', values_to="VALUE") %>%
+  pivot_longer(cols=c(FT, SEED_WEIGHT_100, FECUNDITY), names_to='PHENOTYPE', values_to="VALUE") %>%
   pivot_wider(names_from=Replicate, names_prefix="REP_", values_from='VALUE')
 
 # another way of summarizing trait replicate correlations by year, condition, and phenotype
@@ -27,31 +27,33 @@ replicate_df %>% group_by(PHENOTYPE) %>% summarise('correlation'=cor(REP_1, REP_
 # 'Germination' shouldn't necessarily have a correlation so that's fine
 
 # check out what's causing the miniscule correlations for total-mass (which is the source of low correlations in fec and fit as well)
-replicate_df %>% filter(PHENOTYPE=="TOTAL_MASS") %>%
+replicate_df <- add_generation(replicate_df)
+
+replicate_df %>% filter(PHENOTYPE=="FECUNDITY") %>%
 ggplot(aes(x=REP_1, y=REP_2))+
   geom_point(aes(color=as.factor(Generation), shape=as.factor(Exp_year), group=as.factor(Generation)), size=2) +
   geom_smooth(method='lm') +
-  labs(title="total mass") +
-  theme_bw()
+  labs(title="fecundity") +
+  theme_bw() 
 
-replicate_df %>% filter(PHENOTYPE=="TOTAL_MASS") %>%
+replicate_df %>% filter(PHENOTYPE=="FECUNDITY") %>%
 ggplot(aes(x=REP_1, y=REP_2))+
   geom_point() +
   geom_smooth(method="lm") +
   facet_wrap(Exp_year~Generation, nrow=2)
 
-replicate_df %>%
+raf <- replicate_df %>%
 ggplot(aes(x=REP_1, y=REP_2))+
   geom_point() +
   geom_smooth(method="lm") +
   facet_wrap(Exp_year~PHENOTYPE, scales="free")
-ggsave("derived_trait_replicate_correlation_plots.png")
+ggsave("derived_trait_replicate_correlation_plots.png", raf)
 
 
 ## how to filter...
 ## based on a verylow correlation of total mass between replicates within-year
 # calculate difference between R1 and R2
-replicate_df <- replicate_df %>% filter(PHENOTYPE=="TOTAL_MASS"|PHENOTYPE=="SEED_WEIGHT_100")
+replicate_df <- replicate_df %>% filter(PHENOTYPE=="FECUNDITY"|PHENOTYPE=="SEED_WEIGHT_100")
 replicate_df$rep_diff <- replicate_df$REP_1 - replicate_df$REP_2
 
 #replicate_df %>% group_by(Exp_year, PHENOTYPE) %>% summarise(mean(rep_diff, na.rm=T))
@@ -59,15 +61,15 @@ replicate_df$rep_diff <- replicate_df$REP_1 - replicate_df$REP_2
 
 # plot the distribution of those differences
 ## total mass
-replicate_df %>% filter(PHENOTYPE=="TOTAL_MASS") %>%
+replicate_df %>% filter(PHENOTYPE=="FECUNDITY") %>%
 ggplot() +
   geom_histogram(aes(rep_diff)) +
   facet_wrap(~Exp_year, nrow=2)
 # vals beyond +/-100 seems like a good cutoff point for total mass
 
 # filter the largest differences
-tm_filt <- replicate_df %>% filter(PHENOTYPE=="TOTAL_MASS") %>% filter(rep_diff<100 & rep_diff>-100)
-problem_reps_tm <- replicate_df %>% filter(PHENOTYPE=="TOTAL_MASS") %>% filter(rep_diff>100 | rep_diff< -100)
+tm_filt <- replicate_df %>% filter(PHENOTYPE=="FECUNDITY") %>% filter(rep_diff<100 & rep_diff>-100)
+problem_reps_tm <- replicate_df %>% filter(PHENOTYPE=="FECUNDITY") %>% filter(rep_diff>100 | rep_diff< -100)
 
 
 ## 100-seed weight
