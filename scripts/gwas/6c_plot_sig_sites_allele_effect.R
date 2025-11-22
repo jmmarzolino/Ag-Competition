9304a902d114c2572af1c0cce0addbb9822f507c
Jill Marzolino <jmarz001@ucr.edu>
2025-11-21 17:34:08 -0800

    add custom site-trait allele effect plot to the end

--- scripts/gwas/6c_plot_sig_sites_allele_effect.R
+++ scripts/gwas/6c_plot_sig_sites_allele_effect.R
@@ -102,3 +102,55 @@ for (i in 6:8) {
 
     #dev.off()
 }
+
+
+# for VRN candidate site, also plot allele effect on fecundity
+
+## read in list of gwas sites
+sites <- fread("ASSOC_6_lmm.assoc.clumped")[,1:11]
+# filter to site of interest 
+afs_i <- right_join(afs, sites, by=c("CHR"="CHR", "BP"="BP")) %>% filter(CHR=="chr4H" & BP==584673252)
+
+# filter genotype data to common sites
+gt_filt <- right_join(gt, afs_i, by=c("CHROM"="CHR", "POS"="BP")) 
+
+# divide genotype subset file line IDs by genotype
+tgs <- t(gt_filt)
+a1 <- names(tgs[grep("0\\|0", tgs),])
+a2 <- names(tgs[grep("1\\|1", tgs),])
+a1a2 <- c(names(tgs[grep("1\\|0", tgs),]), names(tgs[grep("0\\|1", tgs),]))
+
+
+a1t <- tibble("A1" = a1)
+a1f <- right_join(ph, a1t, by=c("V1"="A1")) %>% select(all_of("V8"))
+a1f$genotype <- "A1 homozygous"
+
+a2t <- tibble("A2"=a2)
+a2f <- right_join(ph, a2t, by=c("V1"="A2")) %>% select(all_of("V8"))
+a2f$genotype <- "A2 homozygous"
+
+jn <- bind_rows(a1f, a2f)
+
+# there may be no heterozygous genotypes
+# in which case, you can't join that data
+if(length(a1a2)!=0) {
+    a1a2t <- tibble("A1A2"=a1a2)
+    a1a2f <- right_join(ph, a1a2t, by=c("V1"="A1A2")) %>% select(all_of("V8"))
+    a1a2f$genotype <- "heterozygous"
+
+    jn <- bind_rows(jn, a1a2f)
+
+    # set the levels
+    jn$genotype <- factor(jn$genotype, levels=c("A1 homozygous", "heterozygous", "A2 homozygous")) 
+}
+
+
+# count sample numbers
+sample_counts <- jn %>% count(genotype) 
+# difference in average A1 / A2 phenotype
+grp_avg <- jn %>% group_by(genotype) %>% summarise(avg = mean(V8, na.rm=T))
+a1a2_diff <- grp_avg[1, 2] - grp_avg[nrow(grp_avg), 2]
+
+# plot
+g <- ggplot(jn, aes(x=genotype, y= get("V8"))) + geom_boxplot() + stat_n_text() + labs(y="Fecundity", title=paste(afs_i$CHR, afs_i$BP), subtitle=paste0("difference in A1, A2 trait average: ", round(a1a2_diff, 3))) + theme_bw()
+ggsave(paste0("allele_effect/sig_sites_FECUNDITY_", afs_i$CHR, "_", afs_i$BP, ".png"), g)
